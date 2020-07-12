de39ffc844ae3f904fc961a242a27430ed454334
Rutwik Choughule <rutwik110@gmail.com>
2020-07-13 03:52:15 +0530

    added OpenGL Template added Blue Screen

---
+++ 01-Windows/00-Windowing/08-OpenGLTemplate/OGLTemplate.cpp
@@ -0,0 +1,250 @@
+// headers
+#include <windows.h>
+#include <stdio.h>
+
+#include "OGLTemplate.h"
+
+// macros
+#define WIN_WIDTH  800
+#define WIN_HEIGHT 600
+
+// global function declarations
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+// global variables
+bool gbFullscreen   = false;
+bool gbActiveWindow = false;
+
+HWND  ghwnd  = NULL;
+FILE* gpFile = NULL;
+
+DWORD dwStyle;
+WINDOWPLACEMENT wpPrev = { sizeof(WINDOWPLACEMENT) };
+
+
+// WinMain()
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int iCmdShow)
+{
+	// function declarations
+	void initialize(void);
+	void display(void);
+
+	// variable declarations
+	bool bDone = false;
+	WNDCLASSEX wndclass;
+	HWND hwnd;
+	MSG msg;
+	TCHAR szAppName[] = TEXT("MyApp");
+
+	// code
+	// open file for logging
+	if (fopen_s(&gpFile, "RMCLog.txt", "w") != 0)
+	{
+		MessageBox(NULL, TEXT("Cannot open RMCLog.txt file.."), TEXT("Error"), MB_OK | MB_ICONERROR);
+		exit(0);
+	}
+	fprintf(gpFile, "==== Application Started ====\n");
+
+	// initialization of WNDCLASSEX
+	wndclass.cbSize = sizeof(WNDCLASSEX);
+	wndclass.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
+	wndclass.cbClsExtra = 0;
+	wndclass.cbWndExtra = 0;
+	wndclass.lpfnWndProc = WndProc;
+	wndclass.hInstance = hInstance;
+	wndclass.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(RMC_ICON));
+	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndclass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+	wndclass.lpszClassName = szAppName;
+	wndclass.lpszMenuName = NULL;
+	wndclass.hIconSm = LoadIcon(hInstance, MAKEINTRESOURCE(RMC_ICON));
+
+	// register above class
+	RegisterClassEx(&wndclass);
+
+	// get the screen size
+	int width = GetSystemMetrics(SM_CXSCREEN);
+	int height = GetSystemMetrics(SM_CYSCREEN);
+
+	// create window
+	hwnd = CreateWindowEx(WS_EX_APPWINDOW,
+		szAppName,
+		TEXT("OpenGL Template"),
+		WS_OVERLAPPEDWINDOW | WS_CLIPSIBLINGS | WS_VISIBLE,
+		100, //(width / 2) - 400,
+		100, //(height / 2) - 300,
+		WIN_WIDTH,
+		WIN_HEIGHT,
+		NULL,
+		NULL,
+		hInstance,
+		NULL);
+
+	ghwnd = hwnd;
+
+	initialize();
+
+	ShowWindow(hwnd, iCmdShow);
+	SetForegroundWindow(hwnd);
+	SetFocus(hwnd);
+
+	// Game Loop!
+	while (bDone == false)
+	{
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			if (msg.message == WM_QUIT)
+				bDone = true;
+			else
+			{
+				TranslateMessage(&msg);
+				DispatchMessage(&msg);
+			}
+		}
+		else
+		{
+			if (gbActiveWindow == true)
+			{
+				// call update() here for OpenGL rendering
+
+				// call display() here for OpenGL rendering
+				display();
+			}
+		}
+	}
+
+	return((int)msg.wParam);
+}
+
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+	// function declaration
+	void resize(int, int);
+	void uninitialize();
+	void ToggleFullscreen(void);
+
+	// code
+	switch (iMsg)
+	{
+
+	case WM_SETFOCUS:
+		gbActiveWindow = true;
+		break;
+
+	case WM_KILLFOCUS:
+		gbActiveWindow = false;
+		break;
+
+	case WM_SIZE:
+		resize(LOWORD(lParam), HIWORD(lParam));
+		break;
+
+	case WM_KEYDOWN:
+		switch (wParam)
+		{
+		case VK_ESCAPE:
+			DestroyWindow(hwnd);
+			break;
+
+		case 0x46:
+		case 0x66:
+			ToggleFullscreen();
+			break;
+
+		default:
+			break;
+		}
+		break;
+
+	case WM_ERASEBKGND:
+		return(0);
+
+	case WM_CLOSE:
+		DestroyWindow(hwnd);
+		break;
+
+	case WM_DESTROY:
+		uninitialize();
+		PostQuitMessage(0);
+		break;
+	}
+
+	return(DefWindowProc(hwnd, iMsg, wParam, lParam));
+}
+
+
+void ToggleFullscreen(void)
+{
+	// local variables
+	MONITORINFO mi = { sizeof(MONITORINFO) };
+
+	// code
+	if (gbFullscreen == false)
+	{
+		dwStyle = GetWindowLong(ghwnd, GWL_STYLE);
+		if (dwStyle & WS_OVERLAPPEDWINDOW)
+		{
+			if (GetWindowPlacement(ghwnd, &wpPrev) &&
+				GetMonitorInfo(MonitorFromWindow(ghwnd, MONITORINFOF_PRIMARY), &mi))
+			{
+				SetWindowLong(ghwnd, GWL_STYLE, dwStyle & ~WS_OVERLAPPEDWINDOW);
+				SetWindowPos(ghwnd, HWND_TOP,
+					mi.rcMonitor.left,
+					mi.rcMonitor.top,
+					mi.rcMonitor.right - mi.rcMonitor.left,
+					mi.rcMonitor.bottom - mi.rcMonitor.top,
+					SWP_NOZORDER | SWP_FRAMECHANGED);
+			}
+		}
+		ShowCursor(FALSE);
+		gbFullscreen = true;
+	}
+	else
+	{
+		SetWindowLong(ghwnd, GWL_STYLE, dwStyle | WS_OVERLAPPEDWINDOW);
+		SetWindowPlacement(ghwnd, &wpPrev);
+		SetWindowPos(ghwnd, HWND_TOP,
+			0, 0, 0, 0,
+			SWP_NOZORDER | SWP_FRAMECHANGED | SWP_NOMOVE | SWP_NOSIZE | SWP_NOOWNERZORDER);
+		ShowCursor(TRUE);
+		gbFullscreen = false;
+	}
+}
+
+
+void initialize(void)
+{
+	// function declarations
+	void resize(int, int);
+
+	// variable declarations
+
+	// code
+	resize(WIN_WIDTH, WIN_HEIGHT);
+}
+
+void resize(int width, int height)
+{
+	// code
+	if (height == 0)
+		height = 1;
+}
+
+void display(void)
+{
+	// code
+}
+
+void uninitialize(void)
+{
+	// code
+	if (gpFile)
+	{
+		fprintf(gpFile, "==== Application Terminated ====\n");
+		fclose(gpFile);
+		gpFile = NULL;
+	}
+}
+
+
