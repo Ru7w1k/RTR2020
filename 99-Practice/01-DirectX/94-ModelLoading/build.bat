a1facd913f8927b76f2eb1447596181fe9322e88
Rutwik Choughule <rutwik110@gmail.com>
2020-12-25 18:55:34 +0530

    added directX model code

---
+++ 99-Practice/01-DirectX/94-ModelLoading/build.bat
@@ -0,0 +1,9 @@
+cls
+
+cl.exe /c /EHsc /I"C:\Assimp\include" main.cpp
+
+rc.exe main.rc
+
+link.exe /LIBPATH:"C:\Assimp\lib\win32" main.obj main.res user32.lib gdi32.lib /SUBSYSTEM:WINDOWS
+
+main.exe
