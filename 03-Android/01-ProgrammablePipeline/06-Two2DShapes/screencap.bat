8efea87aaae08facd3747da0dc76f8b4275e3a57
Rutwik Choughule <rutwik110@gmail.com>
2021-05-08 02:44:13 +0530

    ✨ [android] added two 2D shapes

---
+++ 03-Android/01-ProgrammablePipeline/06-Two2DShapes/screencap.bat
@@ -0,0 +1,7 @@
+SET app=two2dshapes
+
+adb shell am force-stop com.ru7w1k.%app%
+adb shell am start -n com.ru7w1k.%app%/com.ru7w1k.%app%.MainActivity
+timeout 4
+adb shell screencap /sdcard/screencap.png
+adb pull /sdcard/screencap.png
