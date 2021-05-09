2140c69d3312d82e9e6c16812c8ccff7f6f648e9
Rutwik Choughule <rutwik110@gmail.com>
2021-05-09 14:37:51 +0530

    ✨ [android] added pyramid and cube texture

---
+++ 03-Android/01-ProgrammablePipeline/11-PyramidAndCubeTexture/app/src/main/java/com/ru7w1k/pyramidcubetexture/GLESMacros.java
@@ -0,0 +1,8 @@
+package com.ru7w1k.pyramidcubetexture;
+
+public class GLESMacros {
+    public static final int RMC_ATTRIBUTE_POSITION = 0;
+    public static final int RMC_ATTRIBUTE_COLOR = 1;
+    public static final int RMC_ATTRIBUTE_NORMAL = 2;
+    public static final int RMC_ATTRIBUTE_TEXCOORD = 3;
+}
