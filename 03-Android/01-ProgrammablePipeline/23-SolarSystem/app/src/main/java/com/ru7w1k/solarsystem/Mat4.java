7d0e325fa680596e32f38688adbb33199a6071f1
Rutwik Choughule <rutwik110@gmail.com>
2021-07-17 01:18:05 +0530

    📱[android] solar system

---
+++ 03-Android/01-ProgrammablePipeline/23-SolarSystem/app/src/main/java/com/ru7w1k/solarsystem/Mat4.java
@@ -0,0 +1,26 @@
+package com.ru7w1k.solarsystem;
+
+public class Mat4 {
+
+    private float[] data;
+
+    public Mat4() {
+        data = new float[16];
+    }
+
+    public Mat4(float[] _data) {
+        data = new float[16];
+        for(int i = 0; i < 16; i++)
+            data[i] = _data[i];
+    }
+
+    public float[] get() {
+        return data;
+    }
+
+    public void set(float[] _data) {
+        for(int i = 0; i < 16; i++)
+            data[i] = _data[i];
+    }
+
+}
