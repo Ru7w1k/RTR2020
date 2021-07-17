321f6547c7e6dfcb13b4e0994d0d519afad66c9a
Rutwik Choughule <rutwik110@gmail.com>
2021-07-18 03:00:42 +0530

    ⊞ [win32] added Sinewave

---
+++ 01-Windows/02-ProgrammablePipeline/33-Sinewave/build.bat
@@ -0,0 +1,9 @@
+cls
+
+rc.exe OGLTemplate.rc
+
+cl.exe /c /EHsc /I"C:\glew\include" OGLTemplate.cpp 
+
+link.exe /LIBPATH:"C:\glew\lib\Release\x64" OGLTemplate.obj OGLTemplate.res user32.lib kernel32.lib gdi32.lib
+
+OGLTemplate.exe
