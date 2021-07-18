a19b8e54ce0231359bd4c04a5f0d1f7bf40f5668
Rutwik Choughule <rutwik110@gmail.com>
2021-07-18 13:20:07 +0530

    ⊞ [win32] CUDA sinewave

---
+++ 01-Windows/02-ProgrammablePipeline/34-SinewaveCUDA/build.bat
@@ -0,0 +1,11 @@
+cls
+
+rc.exe OGLTemplate.rc
+
+nvcc -c -o sinewave.obj sinewave.cu
+
+cl.exe /c /EHsc /I"C:\glew\include" /I"C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.3\include" OGLTemplate.cpp 
+
+link.exe /LIBPATH:"C:\glew\lib\Release\x64" /LIBPATH:"C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.3\lib\x64" OGLTemplate.obj OGLTemplate.res sinewave.obj user32.lib kernel32.lib gdi32.lib
+
+OGLTemplate.exe
