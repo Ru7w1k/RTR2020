66e95b00e4b275b88c872db3b23439bf4e9b204d
Rutwik Choughule <rutwik110@gmail.com>
2021-08-01 00:06:35 +0530

    ⊞ [D3D11] Log

---
+++ 07-Direct3D11/01-D3D11Log/D3D11.cpp
@@ -0,0 +1,67 @@
+// headers
+#include <d3d11.h> // ~ <gl/gl.h>
+#include <stdio.h>
+
+// libraries
+#pragma comment(lib, "d3d11.lib")
+#pragma comment(lib, "dxgi.lib")
+
+// main
+int main(void) {
+  // local variable declarations
+  IDXGIFactory *pIDXGIFactory = NULL;
+  IDXGIAdapter *pIDXGIAdapter = NULL; // ~ physical graphics device
+
+  DXGI_ADAPTER_DESC dxgiAdapterDesc;
+  HRESULT hr;
+  char str[255];
+
+  // code
+
+  // coInitialize() and coCreateInstance() is internally called by
+  // CreateDXGIFactory() method
+  hr = CreateDXGIFactory(__uuidof(IDXGIFactory), (void **)&pIDXGIFactory);
+  if (FAILED(hr)) {
+    printf("CreateDXGIFactory() failed..\n");
+    goto CLEANUP;
+  }
+
+  // enumerate all compatible adapters
+  if (pIDXGIFactory->EnumAdapters(0, &pIDXGIAdapter) == DXGI_ERROR_NOT_FOUND) {
+    printf("Cannot find DXGIAdapter..\n");
+    goto CLEANUP;
+  }
+
+  // fetch adapter descre
+  ZeroMemory((void *)&dxgiAdapterDesc, sizeof(DXGI_ADAPTER_DESC));
+  hr = pIDXGIAdapter->GetDesc(&dxgiAdapterDesc);
+  if (FAILED(hr)) {
+    printf("GetDesc() failed..\n");
+    goto CLEANUP;
+  }
+
+  // convert wide char string to ansi string
+  WideCharToMultiByte(CP_ACP, // ANSI CODE PAGE
+                      0,
+                      dxgiAdapterDesc.Description, // source string
+                      255,                         // size of input string
+                      str,                         // destination buffer
+                      255,                         // size of destination buffer
+                      NULL, NULL);
+
+  printf("GPU:      %s\n", str);
+  printf("GPU VRAM: %I64d bytes\n", dxgiAdapterDesc.DedicatedVideoMemory);
+
+CLEANUP:
+  if (pIDXGIAdapter) {
+    pIDXGIAdapter->Release();
+    pIDXGIAdapter = NULL;
+  }
+
+  if (pIDXGIFactory) {
+    pIDXGIFactory->Release();
+    pIDXGIFactory = NULL;
+  }
+
+  return (0);
+}
